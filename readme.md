b88a6e70e50cc707f48f5be11cd97b6e2f550c9d
Okonutnut <darlitocabalse.acad@gmail.com>
2026-01-22 13:22:05 +0800

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# myTasks
+
+undefined
+
+undefined
---
+++ source/class/myTasks/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright: 2026 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("myTasks.theme.Theme",
+{
+  meta :
+  {
+    color : myTasks.theme.Color,
+    decoration : myTasks.theme.Decoration,
+    font : myTasks.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : myTasks.theme.Appearance
+  }
+});
